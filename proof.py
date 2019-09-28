a6631a035c253c6c1117a6590d5ca0a8ca76a9a3
Evan Rupert <evan@studioreach.io>
2019-09-28 13:08:31 -0400

    Add cli argument parsing

--- proof.py
+++ proof.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python3
+import sys
 
 def main():
-    print('Hello, World!')
+    if (len(sys.argv) < 4):
+        print('Incorrect number of arguments')
+        return
+
+    
+    exec_name = sys.argv[1]
+    input_folder = sys.argv[2]
+    output_folder = sys.argv[3]
+
+    print(f'Executable name: {exec_name}')
+    print(f'Input folder: {input_folder}')
+    print(f'Output folder: {output_folder}')
 
 if __name__ == '__main__':
     main()
