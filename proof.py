ddc040b4d7f321fe58a8ae5c54a25ad359e6a3cd
Evan Rupert <evan@studioreach.io>
2019-09-28 13:47:38 -0400

    Add subprocess to execute executable

--- proof.py
+++ proof.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 import sys
+import os
 
 def main():
     if (len(sys.argv) < 4):
         print('Incorrect number of arguments')
         return
 
-    
     exec_name = sys.argv[1]
     input_folder = sys.argv[2]
     output_folder = sys.argv[3]
@@ -15,9 +15,15 @@ def main():
     print(f'Input folder: {input_folder}')
     print(f'Output folder: {output_folder}')
 
+    run_test(exec_name, 'input.in')
+
+
+def run_test(exec_name, input_file):
+    os.system(f'{exec_name} < {input_file} > actual.out')
+
+
 if __name__ == '__main__':
     main()
-
 def print_answers (self, input_file_names):
     outf = open(input_folder + "answers.txt")
     for (file in input_file_names)
